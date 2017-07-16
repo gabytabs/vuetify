8b53678c29a5336a4947e9136e90fb59477771dd
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-07-16 18:35:50 -0400

    transferring files to add more kinds of vuetication

---
+++ 1/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+        <home-app></home-app>
+    </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      msg: "Hello, World!"
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ 1/src/ServerStatus.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p>
+            The message from heaven: {{ msgs }}
+        </p>
+        <button @click="changeMessage">Change the Message</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                msgs: "Hello from Home Component!"
+            }
+        },
+        methods: {
+            changeMessage () {
+                this.msgs = 'Too much yoloswag for me dawg!';
+            }
+        }
+    }
+</script>
