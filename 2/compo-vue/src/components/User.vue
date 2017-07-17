4ebe068c2a2b32db6bd1f50395115e77834544a5
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-07-16 21:12:55 -0400

    test for for loops in vueJS success

--- 2/compo-vue/src/components/User.vue
+++ 2/compo-vue/src/components/User.vue
@@ -5,7 +5,10 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
+                <app-user-detail v-for="sample in samples" v-bind:name="name"
+                v-bind:colorname="sample">
+
+                </app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -19,6 +22,42 @@
     import UserEdit from './UserEdit.vue';
 
     export default {
+        data: function(){
+          return {
+              name: "YoloSwag",
+              samples: {
+                  "colorsArray":[{
+                      "colorName":"red",
+                      "hexValue":"#f00"
+                  },
+                      {
+                          "colorName":"green",
+                          "hexValue":"#0f0"
+                      },
+                      {
+                          "colorName":"blue",
+                          "hexValue":"#00f"
+                      },
+                      {
+                          "colorName":"cyan",
+                          "hexValue":"#0ff"
+                      },
+                      {
+                          "colorName":"magenta",
+                          "hexValue":"#f0f"
+                      },
+                      {
+                          "colorName":"yellow",
+                          "hexValue":"#ff0"
+                      },
+                      {
+                          "colorName":"black",
+                          "hexValue":"#000"
+                      }
+                  ]
+              }
+          }
+        },
         components: {
             appUserDetail: UserDetail,
             appUserEdit: UserEdit
--- 2/compo-vue/src/components/UserDetail.vue
+++ 2/compo-vue/src/components/UserDetail.vue
@@ -2,10 +2,17 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User name: {{ name }}</p>
+        <div v-for="color in colorname">
+            {{ color.colorName }}
+        </div>
     </div>
 </template>
 
 <script>
+    export default {
+        props: ['name', 'colorname']
+    }
 </script>
 
 <style scoped>
