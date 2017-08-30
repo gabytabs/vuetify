513fc1c053db93413f56160943227624cdff4e47
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-08-29 20:12:50 -0400

    router vue

--- router-test/src/App.vue
+++ router-test/src/App.vue
@@ -1,20 +1,19 @@
 <template>
-  <div id="app">
-
-  </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Routing</h1>
+                <hr>
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    export default {
     }
-  }
-}
 </script>
 
-<style lang="scss">
-
+<style>
 </style>
