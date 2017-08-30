2c5c209c862f0bb846b71cf8fe98f92aa446f50c
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-08-29 21:26:15 -0400

    added button with ability to pass params id

--- router-test/src/components/user/UserDetail.vue
+++ router-test/src/components/user/UserDetail.vue
@@ -2,5 +2,12 @@
     <div>
         <h3>Some User Details</h3>
         <p>User loaded has ID: {{ $route.params.id }}</p>
+
+        <router-link
+                tag="button"
+                class="btn btn-primary"
+                :to="'/user/' + $route.params.id  + '/edit'">
+            Edit User
+        </router-link>
     </div>
 </template>
