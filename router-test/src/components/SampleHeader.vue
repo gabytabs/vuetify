658c1cc253402d0aea938e70a0f91a389a03ae72
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-08-29 20:44:01 -0400

    added back button to routers

---
+++ router-test/src/components/SampleHeader.vue
@@ -0,0 +1,10 @@
+<template>
+    <ul class="nav nav-pills">
+        <router-link to="/" tag="li">
+            <a>Home</a>
+        </router-link>
+        <router-link to="/user" tag="li">
+            <a>User</a>
+        </router-link>
+    </ul>
+</template>
--- router-test/src/components/user/User.vue
+++ router-test/src/components/user/User.vue
@@ -1,4 +1,20 @@
 <template>
-    <h1>The User Page</h1>
+    <div>
+        <h1>The User Page</h1>
 
+        <button @click="navigateToHome">
+            Go To Home
+        </button>
+
+    </div>
 </template>
+
+<script>
+    export default {
+        methods: {
+            navigateToHome() {
+                this.$router.push('/');
+            }
+        }
+    }
+</script>
