cf19541f6c90e37868605490d09fee2bf1432e8d
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-09-08 01:41:10 -0400

    setup vuex complete

--- auth-fire/src/main.js
+++ auth-fire/src/main.js
@@ -3,12 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { store } from './store/index'
+import VueBlu from 'vue-blu'
+import 'vue-blu/dist/css/vue-blu.min.css'
+
+Vue.use(VueBlu)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   template: '<App/>',
   components: { App }
---
+++ auth-fire/src/store/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(Vuex, axios, VueAxios)
+
+export const store = new Vuex.Store({
+
+})
