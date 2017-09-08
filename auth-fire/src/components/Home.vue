af9402da37af7585836bc7d9c05e5ea90ac9ab4d
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-09-08 02:58:13 -0400

    success auth

--- auth-fire/src/components/Home.vue
+++ auth-fire/src/components/Home.vue
@@ -1,20 +1,49 @@
 <template>
-  <div>
-    <h1> Hello World </h1>
-  </div>
+  <section class="box login-container">
+    <p class="control has-icon">
+      <input class="input"
+             v-model="emailInput"
+             type="email"
+             placeholder="Email">
+      <i class="fa fa-envelope"></i>
+    </p>
+    <p class="control has-icon">
+      <input class="input"
+             v-model="passwordInput"
+             type="password"
+             placeholder="Password">
+      <i class="fa fa-lock"></i>
+    </p>
+    <p class="control">
+      <button @click="logIn({email: emailInput, password: passwordInput})"
+              class="button is-success">
+        Login
+      </button>
+    </p>
+  </section>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
   data () {
     return {
-
+      emailInput: '',
+      passwordInput: ''
     }
+  },
+  methods: {
+    ...mapActions({
+      logIn: 'logIn'
+    })
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .login-container{
+    margin-top: 100px;
+  }
 </style>
--- auth-fire/src/store/index.js
+++ auth-fire/src/store/index.js
@@ -2,9 +2,51 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { firebaseMutations, firebaseAction } from 'vuexfire'
+import firebase from 'firebase'
 
 Vue.use(Vuex, axios, VueAxios)
 
+const db = firebase.initializeApp({
+  apiKey: 'AIzaSyA2ydRYtNfe-EjubA84gl6DU73zq5pJv5M',
+  authDomain: 'vue-f-c5a38.firebaseapp.com',
+  databaseURL: 'https://vue-f-c5a38.firebaseio.com',
+  projectId: 'vue-f-c5a38',
+  storageBucket: 'vue-f-c5a38.appspot.com',
+  messagingSenderId: '771751348724'
+}).database()
+
+export const todosRef = db.ref('todos')
+
 export const store = new Vuex.Store({
 
+  strict: true,
+
+  state: {
+    todos: [], // Will be bound as an array
+    user: null // Will be bound as an object
+  },
+
+  mutations: {
+    ...firebaseMutations
+  },
+
+  getters: {
+    todos: state => state.todos
+  },
+
+  actions: {
+    setTodosRef: firebaseAction(({ bindFirebaseRef }, ref) => {
+      bindFirebaseRef('todos', ref)
+    }),
+    logIn: function (context, payload) {
+      firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
 })
