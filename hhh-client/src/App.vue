de19614dad42fde647863443b5f5c422651c5531
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-09-29 12:32:21 -0400

    init the gitfire

--- hhh-client/src/App.vue
+++ hhh-client/src/App.vue
@@ -1,29 +1,17 @@
 <template>
   <v-app dark>
-    <!-- Navigation -->
-    <v-toolbar class="deep-purple darken-1">
-      <v-toolbar-side-icon></v-toolbar-side-icon>
-      <v-toolbar-title>HHH</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>search</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>apps</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>refresh</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <!-- End of Nav component-->
-    <main>
-
-    </main>
+     <Navbar> </Navbar>
+    <router-view></router-view>
   </v-app>
 </template>
 
 <script>
+  import Navbar from './components/Navbar.vue'
+
   export default {
+    components: {
+      Navbar
+    },
     data () {
       return {
 
---
+++ hhh-client/src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+  <v-navigation-drawer class="purple lighten-4" v-model="sideNav">
+    <v-list class="pa-1">
+      <v-list-tile>
+        <v-list-tile-action>
+          <v-icon>account_balance</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          Hey!
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+  <!-- Navigation -->
+  <v-toolbar class="deep-purple darken-1">
+    <!-- Side Icon -->
+    <v-toolbar-side-icon
+      @click.native.stop="sideNav = !sideNav">
+    </v-toolbar-side-icon>
+    <!-- End of Side Icon-->
+    <v-toolbar-title>HHH</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon>search</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>apps</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>refresh</v-icon>
+    </v-btn>
+  </v-toolbar>
+  <!-- End of Nav component-->
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        sideNav: false
+      }
+    }
+  }
+</script>
