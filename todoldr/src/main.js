f62061005d3594df34d2c8c461af22f3e2e9760b
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-10-12 21:41:42 -0400

    added vuex and firebase

--- todoldr/src/main.js
+++ todoldr/src/main.js
@@ -3,12 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { store } from './store/index'
 
 Vue.config.productionTip = true
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   template: '<App/>',
   components: { App }
